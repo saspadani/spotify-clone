ca33a47ceb4a8892239460e010379fb547f6ccf0
saspadani <aspadani@gmail.com>
2023-06-14 16:48:59 +0700

    layout and header

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,10 @@ import { Figtree } from 'next/font/google';
 const font = Figtree({ subsets: ['latin'] });
 
 import Sidebar from '@/components/Sidebar';
+import SupabaseProvider from '@/providers/SupabaseProvider';
+import UserProvider from '@/providers/UserProvider';
+import ModalProvider from '@/providers/ModalProvider';
+import ToasterProvider from '@/providers/ToasterProvider';
 
 export const metadata = {
   title: 'Spotify Clone',
@@ -18,7 +22,13 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={font.className}>
-        <Sidebar>{children}</Sidebar>
+        <ToasterProvider />
+        <SupabaseProvider>
+          <UserProvider>
+            <ModalProvider />
+            <Sidebar>{children}</Sidebar>
+          </UserProvider>
+        </SupabaseProvider>
       </body>
     </html>
   );
