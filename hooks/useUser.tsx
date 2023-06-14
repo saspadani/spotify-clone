3c7c2c31797facf29336d63523b9fe63d38d877d
saspadani <aspadani@gmail.com>
2023-06-14 16:49:32 +0700

    custom hook

---
+++ hooks/useUser.tsx
@@ -0,0 +1,89 @@
+import { Subcription, UserDetails } from '@/types';
+import { User } from '@supabase/auth-helpers-nextjs';
+import {
+  useSessionContext,
+  useUser as useSupaUser,
+} from '@supabase/auth-helpers-react';
+import { createContext, useContext, useEffect, useState } from 'react';
+
+type UserContextType = {
+  accessToken: string | null;
+  user: User | null;
+  userDetails: UserDetails | null;
+  isLoading: boolean;
+  subcription: Subcription | null;
+};
+
+export const UserContext = createContext<UserContextType | undefined>(
+  undefined
+);
+
+export interface Props {
+  [propName: string]: any;
+}
+
+export const MyUserContextProvider = (props: Props) => {
+  const {
+    session,
+    isLoading: isLoadingUser,
+    supabaseClient: supabase,
+  } = useSessionContext();
+
+  const user = useSupaUser();
+  const accessToken = session?.access_token ?? null;
+  const [isLoadingData, setIsLoadingData] = useState(false);
+  const [userDetails, setUserDetails] = useState<UserDetails | null>(null);
+  const [subcription, setSubcription] = useState<Subcription | null>(null);
+
+  const getUserDetails = () => supabase.from('users').select('*').single();
+  const getSubcription = () =>
+    supabase
+      .from('subcriptions')
+      .select('*, prices(*, products(*))')
+      .in('status', ['trialing', 'active'])
+      .single();
+
+  useEffect(() => {
+    if (user && !isLoadingData && !userDetails && !subcription) {
+      setIsLoadingData: true;
+
+      Promise.allSettled([getUserDetails(), getSubcription()]).then(
+        (result) => {
+          const userDetailsPromise = result[0];
+          const subcriptionPromise = result[1];
+
+          if (userDetailsPromise.status === 'fulfilled') {
+            setUserDetails(userDetailsPromise.value.data as UserDetails);
+          }
+
+          if (subcriptionPromise.status === 'fulfilled') {
+            setSubcription(subcriptionPromise.value.data as Subcription);
+          }
+
+          setIsLoadingData(false);
+        }
+      );
+    } else if (!user && !isLoadingUser && !isLoadingData) {
+      setUserDetails(null);
+      setSubcription(null);
+    }
+  }, [user, isLoadingUser]);
+
+  const value = {
+    accessToken,
+    user,
+    userDetails,
+    isLoading: isLoadingUser || isLoadingData,
+    subcription,
+  };
+
+  return <UserContext.Provider value={value} {...props} />;
+};
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (context === undefined) {
+    throw new Error('useUser must be used within a UserProvider');
+  }
+  return context;
+};
