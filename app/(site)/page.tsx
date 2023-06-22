07c2bb08db84d79f814e09ffbd43fead9599a2f6
saspadani <aspadani@gmail.com>
2023-06-22 13:41:54 +0700

    Home web

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,13 @@
+import getSongs from '@/actions/getSongs';
 import Header from '@/components/Header';
 import ListItem from '@/components/ListItem';
+import PageContent from './components/PageContent';
 
-export default function Home() {
+export const revalidate = 0; //this page will not be cache, data always update
+
+export default async function Home() {
+  const songs = await getSongs();
+
   return (
     <div className='bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto'>
       <Header>
@@ -20,7 +26,7 @@ export default function Home() {
         <div className='flex items-center justify-between'>
           <h1 className='text-2xl text-white font-semibold'>Newest songs</h1>
         </div>
-        <div>List of Songs!</div>
+        <PageContent songs={songs} />
       </div>
     </div>
   );
